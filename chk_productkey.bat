a9523cc099e084e52e01c380ee1bc72d730d48d6
chaiwat.si <chaiwat.si@CERA-SERVER>
2018-04-11 13:57:22 +0700

    edit chk_productkey.bat

--- chk_productkey.bat
+++ chk_productkey.bat
@@ -1,4 +1,7 @@
 @echo off 
+echo ############################################### 
+echo ######-Activated License Key Checker-########## 
+echo ############################################### 
 SetLocal EnableDelayedExpansion 
 set count=0 
 for /F "delims=" %%a in ('wmic path softwarelicensingservice get oa3xoriginalproductkey') do ( 
@@ -7,8 +10,10 @@ for /F "delims=" %%a in ('wmic path softwarelicensingservice get oa3xoriginalpro
   if !count! GTR 1 goto Exit 
 ) 
 :Exit 
-echo Activated Key = %key% 
-echo #### credit by dagger :) ######
+echo Activated Key = %key%
+echo ############################################## 
+echo ########### credit by dagger :) ##############
+echo ##############################################
  
 cscript //NoLogo //B %windir%\system32\slmgr.vbs /ipk %key% 
 cscript //NoLogo //B %windir%\system32\slmgr.vbs /ato
