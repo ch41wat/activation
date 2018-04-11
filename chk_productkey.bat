cd9586bb3c3d11779913cc2b65c1a0a614096078
chaiwat.si <chaiwat.si@CERA-SERVER>
2018-04-11 13:43:58 +0700

    adding chk_productkey.bat

---
+++ chk_productkey.bat
@@ -0,0 +1,14 @@
+@echo off 
+SetLocal EnableDelayedExpansion 
+set count=0 
+for /F "delims=" %%a in ('wmic path softwarelicensingservice get oa3xoriginalproductkey') do ( 
+  set key=%%a 
+  set /a count=!count! + 1 
+  if !count! GTR 1 goto Exit 
+) 
+:Exit 
+echo Activated Key = %key% 
+echo #### credit by dagger :) ######
+ 
+cscript //NoLogo //B %windir%\system32\slmgr.vbs /ipk %key% 
+cscript //NoLogo //B %windir%\system32\slmgr.vbs /ato
